503ff4020d2c45b9288ef220603eed15127c64d6
Donald Pakkies <donald@sov.tech>
2019-12-12 10:09:59 +0200

    feat: Boot up core services
    create services

--- src/Concepts/CoreServices.php
+++ src/Concepts/CoreServices.php
@@ -23,4 +23,22 @@ class CoreServices
    * @var \App\Resolvers\AppServiceResolver:
    */
   protected $appServiceResolver;
+
+  /**
+   * Boot up core services
+   *
+   * @param \Modulus\Upstart\Application $app
+   * @param \App\Http\HttpFoundation $foundation
+   * @param \App\Exceptions\Handler $handler
+   * @param \App\Resolvers\RouterResolver $routerResolver
+   * @param \App\Resolvers\AppServiceResolver $appServiceResolver
+   * @return void
+   */
+  public function __construct($app, $foundation = null, $handler = null, $routerResolver = null, $appServiceResolver = null)
+  {
+    $this->httpFoundation     = $foundation && class_exists($foundation) ? new $foundation : null;
+    $this->handler            = $handler && class_exists($handler) ? new $handler : null;
+    $this->routerResolver     = $routerResolver && class_exists($routerResolver) ? new $routerResolver : null;
+    $this->appServiceResolver = $appServiceResolver && class_exists($appServiceResolver) ? (new $appServiceResolver($app)) : null;
+  }
 }
