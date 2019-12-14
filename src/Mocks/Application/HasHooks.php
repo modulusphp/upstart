f9be3334a99c0a851fa1a31ded17c683990d8835
Donald Pakkies <donald@sov.tech>
2019-12-14 12:25:24 +0200

    feat: Application hooks

---
+++ src/Mocks/Application/HasHooks.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Modulus\Upstart\Mocks\Application;
+
+use Modulus\Upstart\Application;
+
+trait HasHooks
+{
+  /**
+   * Application hooks
+   *
+   * @var array $hooks
+   */
+  private static $hooks = [];
+}
