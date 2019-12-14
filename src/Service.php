520faab56683252f63085ff2e39a7c85b50a28a4
Donald Pakkies <donald@sov.tech>
2019-12-14 13:41:21 +0200

    feat: Application container

---
+++ src/Service.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Modulus\Upstart;
+
+use Exception;
+use Modulus\Upstart\Driver;
+use Modulus\Upstart\Application;
+
+class Service
+{
+  /**
+   * Application container
+   *
+   * @var Application $app
+   */
+  protected $app;
+}
