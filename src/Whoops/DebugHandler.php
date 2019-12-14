949bb89e9b570ab07b7fb6dd4fdaacd8819b0433
Donald Pakkies <donald@sov.tech>
2019-12-14 18:39:50 +0200

    feat: set application exception

---
+++ src/Whoops/DebugHandler.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Modulus\Upstart\Whoops;
+
+use Modulus\Upstart\Response;
+use Whoops\Handler\PrettyPageHandler;
+
+class DebugHandler extends PrettyPageHandler
+{
+  /**
+   * {@inheritDoc}
+   */
+  private $exception;
+
+  /**
+   * {@inheritDoc}
+   */
+  public function setException($exception)
+  {
+    $this->exception = Response::prepare($exception);
+  }
+}
