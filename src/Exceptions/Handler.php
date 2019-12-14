d228c647ad126cb65556d531273f7c0c4d0e200c
Donald Pakkies <donald@sov.tech>
2019-12-14 12:55:04 +0200

    feat: Expects rest

--- src/Exceptions/Handler.php
+++ src/Exceptions/Handler.php
@@ -22,4 +22,29 @@ class Handler
    * @var string
    */
   protected const ERROR_500_TITLE = 'Oops, something is not working';
+
+  /**
+   * Expects rest
+   *
+   * @param Request $request
+   * @return bool
+   */
+  private function shouldRest($request) : bool
+  {
+    if (
+      ($request->headers->has('Accept') && str_contains(strtolower($request->headers->accept), ['json', 'javascript'])) ||
+      (
+        (
+          $request->headers->has('Content-Type') &&
+          !str_contains(strtolower($request->headers->contenttype), ['json', 'javascript'])
+        ) &&
+        !isset($request->headers->all()['Accept'])
+      ) ||
+      $request->isAjax()
+    ) {
+      return true;
+    }
+
+    return false;
+  }
 }
