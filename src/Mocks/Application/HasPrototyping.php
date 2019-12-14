65b8805cb6d5b6feaef5f4c28f9ed3579a2c4e6b
Donald Pakkies <donald@sov.tech>
2019-12-14 13:10:27 +0200

    feat: Add custom static function

--- src/Mocks/Application/HasPrototyping.php
+++ src/Mocks/Application/HasPrototyping.php
@@ -32,4 +32,26 @@ trait HasPrototyping
 
     return $class::bind($method, $closure);
   }
+
+  /**
+   * Add custom static function
+   *
+   * @param string $class
+   * @param string $method
+   * @param Closure $closure
+   * @return mixed
+   */
+  public function static(string $class, string $method, Closure $closure)
+  {
+    if (!class_exists($class)) throw new ClassDoesNotExistException("Class {$class} does not exist");
+
+    if (!in_array(
+      Extendable::class,
+      array_keys((new ReflectionClass($class))->getTraits()))
+    ) {
+      throw new CannotExtendClassException("Cannot extend \"{$class}::class\"");
+    }
+
+    return $class::static($method, $closure);
+  }
 }
