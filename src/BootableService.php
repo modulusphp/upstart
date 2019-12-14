1d051d8c110dec02bc4d44abc2137f1c9f45ff01
Donald Pakkies <donald@sov.tech>
2019-12-14 12:23:38 +0200

    feat: Instantiate class

--- src/BootableService.php
+++ src/BootableService.php
@@ -7,6 +7,13 @@ use Modulus\Upstart\Application;
 class BootableService
 {
   /**
+   * Instantiate class
+   *
+   * @var bool
+   */
+  protected const CREATE = true;
+
+  /**
    * Boot service
    *
    * @return array|void $this->hook
