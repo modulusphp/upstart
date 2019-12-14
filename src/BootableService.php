89a9938cb770aac9a1619ae94581c499f92ae686
Donald Pakkies <donald@sov.tech>
2019-12-14 12:23:14 +0200

    feat: Boot service

---
+++ src/BootableService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Modulus\Upstart;
+
+use Modulus\Upstart\Application;
+
+class BootableService
+{
+  /**
+   * Boot service
+   *
+   * @return array|void $this->hook
+   */
+  protected function boot()
+  {
+    //
+  }
+}
