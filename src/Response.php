78e3372943ff896909f6fe588fc373c024eab3f9
Donald Pakkies <donald@sov.tech>
2019-12-14 18:35:17 +0200

    feat: build and get application response

---
+++ src/Response.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Modulus\Upstart;
+
+use Exception;
+use Modulus\Http\Rest;
+use Modulus\Utility\View;
+use Modulus\Http\Redirect;
+use Modulus\Utility\Groupable;
+
+class Response
+{
+  /**
+   * Get application response
+   *
+   * @param mixed $response
+   * @return mixed
+   */
+  public static function build($response, $console = false)
+  {
+    /** Convert to collection */
+    if ($response instanceof Groupable) {
+      $response = $response->all();
+    }
+
+    /** Convert to array */
+    if (method_exists($response, 'toArray')) {
+      $response = $response->toArray();
+    }
+
+    if ($console) return $response;
+
+    /** Create a rest response */
+    if (
+      is_string($response) ||
+      is_int($response) ||
+      is_float($response) ||
+      is_double($response) ||
+      is_array($response)
+    ) return is_array($response) ? Rest::response()->json($response)->send() : Rest::response($response)->send();
+
+    if (is_bool($response)) return Rest::response($response ? 'true' : 'false')->send();
+
+    if ($response instanceof Rest) return $response->send();
+
+    /** Create a redirect */
+    if ($response instanceof Redirect) return $response->send();
+
+    /** Create a view page */
+    if (Response::isView($response)) return;
+
+    /** Avoid "Segmentation fault (core dumped)" */
+    echo ' ';
+
+    /** Return nothing */
+    return null;
+  }
+
+  /**
+   * Render a view
+   *
+   * @param mixed $response
+   * @return bool
+   */
+  public static function isView($response) : bool
+  {
+    if ($response instanceof View) {
+      $response->render();
+
+      return true;
+    }
+
+    return false;
+  }
+}
