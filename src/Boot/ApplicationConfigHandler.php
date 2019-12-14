b86f78c0a733a9b1dde498bc8a36ad208028bb88
Donald Pakkies <donald@sov.tech>
2019-12-14 13:54:39 +0200

    feat: Set application config path

---
+++ src/Boot/ApplicationConfigHandler.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Modulus\Upstart\Boot;
+
+use Modulus\Hibernate\Config\Cache;
+use Modulus\Upstart\BootableService;
+use Modulus\Hibernate\Config\BaseConfig;
+
+class ApplicationConfigHandler extends BootableService
+{
+  use Cache;
+
+  /**
+   * Set application config path
+   *
+   * @param string $root
+   * @return void
+   */
+  public function __construct(string $root, bool $autoCache = false)
+  {
+    $this->config    = $root . 'config';
+    $this->bootstrap = $root . 'bootstrap';
+    $this->autoCache = $autoCache;
+  }
+}
