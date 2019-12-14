00b14d7e48ba2e7b95b63e6b08b9fab827fcfa57
Donald Pakkies <donald@sov.tech>
2019-12-14 13:14:09 +0200

    feat: Application response

---
+++ src/Mocks/Application/HasResponse.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Modulus\Upstart\Mocks\Application;
+
+use Modulus\Http\Request;
+
+trait HasResponse
+{
+  /**
+   * Application response
+   *
+   * @var mixed
+   */
+  private $response;
+}
