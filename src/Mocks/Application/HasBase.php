562d475dd889c94382dab18801e6651b4b48d8c8
Donald Pakkies <donald@sov.tech>
2019-12-14 12:18:30 +0200

    feat: Return application root

--- src/Mocks/Application/HasBase.php
+++ src/Mocks/Application/HasBase.php
@@ -26,4 +26,26 @@ trait HasBase
 
     define('APP_ROOT', $this->root);
   }
+
+  /**
+   * Return root
+   *
+   * @param string|null $path
+   * @return string
+   */
+  public function root(string $path = null)
+  {
+    return $this->root . ($path ? ltrim($path, '/') : '');
+  }
+
+  /**
+   * Return root
+   *
+   * @param string|null $path
+   * @return string
+   */
+  public function getRoot(string $path = null)
+  {
+    return $this->root . ($path ? ltrim($path, '/') : '');
+  }
 }
