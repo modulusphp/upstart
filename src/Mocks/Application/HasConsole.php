623880b1bab7efc354cda6d56ea923fab3661975
Donald Pakkies <donald@sov.tech>
2019-12-14 13:07:27 +0200

    feat: Set application console

---
+++ src/Mocks/Application/HasConsole.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Modulus\Upstart\Mocks\Application;
+
+use Exception;
+use Modulus\Console\Kernel;
+
+trait HasConsole
+{
+  /**
+   * Application console
+   *
+   * @var Kernel
+   */
+  public $kernel;
+
+  /**
+   * Set application console
+   *
+   * @param Kernel $kernel
+   * @return mixed
+   */
+  public function console(string $kernel)
+  {
+  if (class_exists($kernel)) {
+    return $this->kernel = $kernel::console();
+  }
+
+    throw new Exception('Kernel does not exist');
+  }
+}
