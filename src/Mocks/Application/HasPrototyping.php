42bd50de72d874d3a2a0944934904bad980f4ba0
Donald Pakkies <donald@sov.tech>
2019-12-14 13:10:07 +0200

    feat: Add custom function

---
+++ src/Mocks/Application/HasPrototyping.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Modulus\Upstart\Mocks\Application;
+
+use Closure;
+use ReflectionClass;
+use ReflectionFunction;
+use Modulus\Support\Extendable;
+use Modulus\Framework\Exceptions\CannotExtendClassException;
+use Modulus\Framework\Exceptions\ClassDoesNotExistException;
+
+trait HasPrototyping
+{
+  /**
+   * Add custom function
+   *
+   * @param string $class
+   * @param string $method
+   * @param Closure $closure
+   * @return mixed
+   */
+  public function bind(string $class, string $method, Closure $closure)
+  {
+    if (!class_exists($class)) throw new ClassDoesNotExistException("Class {$class} does not exist");
+
+    if (!in_array(
+      Extendable::class,
+      array_keys((new ReflectionClass($class))->getTraits()))
+    ) {
+      throw new CannotExtendClassException("Cannot extend \"{$class}::class\"");
+    }
+
+    return $class::bind($method, $closure);
+  }
+}
