f25b322c15c6725906040f11336629962e206a0e
Donald Pakkies <donald@sov.tech>
2019-12-14 13:44:02 +0200

    feat: This namespace is applied to your controller routes.

---
+++ src/Resolvers/Router/Service.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Modulus\Upstart\Resolvers\Router;
+
+use Modulus\Http\Route;
+use Modulus\Upstart\Application;
+
+class Service
+{
+  /**
+   * This namespace is applied to your controller routes.
+   *
+   * In addition, it is set as the URL generator's root namespace.
+   *
+   * @var string
+   */
+  protected $namespace = 'App\Http\Controllers';
+}
