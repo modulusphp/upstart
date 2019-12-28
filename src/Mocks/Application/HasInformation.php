c5953e84812ee0bd76aa56a49f98a293c90092ca
Donald Pakkies <donald@sov.tech>
2019-12-28 08:08:47 +0200

    feat: Composer file

--- src/Mocks/Application/HasInformation.php
+++ src/Mocks/Application/HasInformation.php
@@ -4,5 +4,16 @@ namespace Modulus\Upstart\Mocks\Application;
 
 trait HasInformation
 {
-  //
+  /**
+   * Composer file
+   *
+   * @param string $ds Directory Seperator
+   * @return string
+   */
+  private function __composer(string $ds = DIRECTORY_SEPARATOR)
+  {
+    $upstart = __DIR__ . $ds . '..' . $ds . '..' . $ds . '..' . $ds . 'composer.json';
+
+    return file_exists('composer.json') ? 'composer.json' : $upstart;
+  }
 }
