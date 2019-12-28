11483d512bc798a68b18fb259fec3c4592fb7fae
Donald Pakkies <donald@sov.tech>
2019-12-28 07:52:52 +0200

    feat: A list of registered plugins

---
+++ src/Mocks/Application/HasPlugins.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modulus\Upstart\Mocks\Application;
+
+trait HasPlugins
+{
+  /**
+   * A list of registered plugins
+   *
+   * @var array $plugins
+   */
+  protected $plugins = [];
+}
